7cd47dea7f7744a1def0d3203889b247ddc2fcd7
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2025-02-24 17:53:06 +0100

    correction 01_download

--- checkmk/roles/05_checkmk_config/files/checkmk-config.sh
+++ checkmk/roles/05_checkmk_config/files/checkmk-config.sh
@@ -5,13 +5,13 @@ source /omd/sites/monitoring/.profile
 
 # Define monitored hosts (Hostname, IP, Description)
 HOSTS=(
-  "server1 192.168.1.101 Ansible Server"
-  "server2 192.168.1.102 Checkmk Server"
-  "server3 192.168.1.103 Zammad Server"
-  "server4 192.168.1.104 Jenkins"
-  "server5 192.168.1.105 SIEM"
-  "server6 192.168.1.106 DNS"
-  "server7 192.168.1.107 DHCP"
+#  "server1 192.168.1.101 Ansible Server"
+  "server2 192.168.1.245 Checkmk Server"
+#  "server3 192.168.1.103 Zammad Server"
+#  "server4 192.168.1.104 Jenkins"
+#  "server5 192.168.1.105 SIEM"
+#  "server6 192.168.1.106 DNS"
+#  "server7 192.168.1.107 DHCP"
 )
 
 # Loop through and add hosts to Checkmk
